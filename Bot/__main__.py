5bc11c0dd5b69d88617f18b329e35b303a35ed0c
Akhil Raj R S <akhilraj3200@gmail.com>
2026-02-08 17:03:37 +0530

    initals

--- Bot/__main__.py
+++ Bot/__main__.py
@@ -2,9 +2,9 @@ from agent import CatalogRouter, DSPyProductCatalog
 from tools import fetch_category_info,fetch_product_info
 import os
 import dspy
-# os.environ["OPENAI_API_KEY"] = "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"
-os.environ["HUGGINGFACE_API_KEY"] = "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"
-os.environ["GROQ_API_KEY"] = "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"
+
+
+
 # agent = dspy.ReAct(
 #     DSPyProductCatalog,
 #     tools = [
